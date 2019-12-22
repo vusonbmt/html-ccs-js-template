32951e497c17bcd05560c231ea2b0fc53f0c655f
Son Vu <vusonbmt@gmail.com>
2019-12-22 20:49:57 +0700

    Update radio button

--- project/js/script.js
+++ project/js/script.js
@@ -1,2 +1,39 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+// var x = document.getElementById("myBtn");
+// if (x.addEventListener) {
+//   x.addEventListener("click", myFunction);
+// } else if (x.attachEvent) {
+//   x.attachEvent("onclick", myFunction);
+// }
+
+// function myFunction() {
+//   alert("Hello World!");
+// }
+
+// function myFunction() {
+//     var coffee = document.forms[0];
+//     var txt = "";
+//     var i;
+//     for (i = 0; i < coffee.length; i++) {
+//       if (coffee[i].checked) {
+//         txt = txt + coffee[i].value + " ";
+//       }
+//     }
+//     document.getElementById("order").value = "You ordered a coffee with: " + txt;
+//   }
+
+// $(document).ready(function(){
+//     $("input[type='button']").click(function(){
+//         var radioValue = $("input[name='gender']:checked").val();
+//         if(radioValue){
+//             alert("Your are a - " + radioValue);
+//         }
+//     });
+// });
+$(document).ready(function() {
+    $("input[name$='cars']").click(function() {
+        var test = $(this).val();
+
+        $("div.desc").hide();
+        $("#Cars" + test).show();
+    });
+});
